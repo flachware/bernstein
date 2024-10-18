c4ed594c37a1cb6891e7a7b49fffd8932c506c4c
Flachware <mail@flachware.com>
2024-10-18 14:32:35 +0200

    Fit terms to curve

--- assets/javascripts/elements/b-app.js
+++ assets/javascripts/elements/b-app.js
@@ -15,6 +15,7 @@ export class App extends CustomElement {
     this.y3 = 0
     this.width = this.x3 - this.x0
     this.height = this.y0 - this.y3
+    this.res = 500
 
     this.update()
   }
@@ -23,14 +24,29 @@ export class App extends CustomElement {
     super.doRender()
   }
 
-  plot(f, b, axis) {
-    let size = axis == 'y' ? this.width : this.height
+  plotx(f, b) {
     let coords = ""
 
-    for (let i = 0; i <= size; i++) {
-      let j = i / size
+    for (let i = 0; i <= this.res; i++) {
+      let j = i / this.res
+      let y = bernstein(this.y0, this.y1, this.y2, this.y3, j)
+      //let y = j * this.res
 
-      coords += ` ${j * size},${f(j, b)}`
+      coords += ` ${y},${f(j, b)}`
+    }
+
+    return coords
+  }
+
+  ploty(f, b) {
+    let coords = ""
+
+    for (let i = 0; i <= this.res; i++) {
+      let j = i / this.res
+      let x = bernstein(this.x0, this.x1, this.x2, this.x3, j)
+      //let x = j * this.res
+
+      coords += ` ${x},${f(j, b)}`
     }
 
     return coords
@@ -45,17 +61,18 @@ export class App extends CustomElement {
               <line x1="0" y1="0" x2="500" y2="0" stroke="#fff" stroke-width="2" stroke-linecap="round" />
               <line x1="0" y1="0" x2="0" y2="500" stroke="#fff" stroke-width="2" stroke-linecap="round" />
             </g>
-            <g class="x-plots" transform="translate(0, ${this.y0}) rotate(-90, 0, 0)">
-              <polyline points="${this.plot(f0, this.x0, 'x')}" fill="none" stroke="#ff0000" stroke-width="2" stroke-linecap="round" />
-              <polyline points="${this.plot(f1, this.x1, 'x')}" fill="none" stroke="#ff00ff" stroke-width="2" stroke-linecap="round" />
-              <polyline points="${this.plot(f2, this.x2, 'x')}" fill="none" stroke="#0000ff" stroke-width="2" stroke-linecap="round" />
-              <polyline points="${this.plot(f3, this.x3, 'x')}" fill="none" stroke="#00ffff" stroke-width="2" stroke-linecap="round" />
+            <g class="x-plots" transform="scale(-1,1) rotate(90 0 0)">
+            <!--<g class="x-plots" transform="translate(0 ${this.height}) rotate(-90 0 0)">-->
+              <polyline points="${this.plotx(f0, this.x0)}" fill="none" stroke="#ff0000" stroke-width="2" stroke-linecap="round" />
+              <polyline points="${this.plotx(f1, this.x1)}" fill="none" stroke="#ff00ff" stroke-width="2" stroke-linecap="round" />
+              <polyline points="${this.plotx(f2, this.x2)}" fill="none" stroke="#0000ff" stroke-width="2" stroke-linecap="round" />
+              <polyline points="${this.plotx(f3, this.x3)}" fill="none" stroke="#00ffff" stroke-width="2" stroke-linecap="round" />
             </g>
-            <g class="y-plots" transform="translate(${this.x0}, 0)">
-              <polyline points="${this.plot(f0, this.y0, 'y')}" fill="none" stroke="#ff0000" stroke-width="2" stroke-linecap="round" />
-              <polyline points="${this.plot(f1, this.y1, 'y')}" fill="none" stroke="#ff00ff" stroke-width="2" stroke-linecap="round" />
-              <polyline points="${this.plot(f2, this.y2, 'y')}" fill="none" stroke="#0000ff" stroke-width="2" stroke-linecap="round" />
-              <polyline points="${this.plot(f3, this.y3, 'y')}" fill="none" stroke="#00ffff" stroke-width="2" stroke-linecap="round" />
+            <g class="y-plots">
+              <polyline points="${this.ploty(f0, this.y0)}" fill="none" stroke="#ff0000" stroke-width="2" stroke-linecap="round" />
+              <polyline points="${this.ploty(f1, this.y1)}" fill="none" stroke="#ff00ff" stroke-width="2" stroke-linecap="round" />
+              <polyline points="${this.ploty(f2, this.y2)}" fill="none" stroke="#0000ff" stroke-width="2" stroke-linecap="round" />
+              <polyline points="${this.ploty(f3, this.y3)}" fill="none" stroke="#00ffff" stroke-width="2" stroke-linecap="round" />
             </g>
             <path d="M ${this.x0},${this.y0} C${this.x1},${this.y1} ${this.x2},${this.y2} ${this.x3},${this.y3}" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" />
             <circle cx="${this.x0}" cy="${this.y0}" fill="#ff0000" r="6" />
